a7ee53cee299a7aad8bf358cb9985831e5936c10
Navashub <navasmuller01@gmail.com>
2023-09-28 12:34:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+if (main) {
+    main.remove();
+};
+
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "NAVAS is the champion";
